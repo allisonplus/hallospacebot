d971f696dfc017bf96e614c512c35ab338786256
Allison Tarr <allison@webdevstudios.com>
2017-01-07 15:12:01 -0500

    add sample config file

---
+++ sample-config.js
@@ -0,0 +1,11 @@
+/*
+	Rename this file to config.js and fill it out
+*/
+
+module.exports = {
+	consumer_key:         'get-your-own-key',
+	consumer_secret:      'and-your-own-secret',
+	access_token:         'a-token-of-your-own',
+	access_token_secret:  'shhh-secrets',
+	key:  'an-api-key'
+}
