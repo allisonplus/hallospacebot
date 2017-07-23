b207c50c23ea5370c85f0e49d0744fa21c2d3fb4
Allison Tarr <hello@allisontarr.com>
2017-07-23 12:33:42 -0400

    add TODO's, set up some basic structure for responses + getting basic media URL + account variables

--- convo.js
+++ convo.js
@@ -1,4 +1,4 @@
-console.log('The image bot is starting');
+console.log( 'Image bot functionality initializing...' );
 
 //Require .env NPM package
 require('dotenv').config();
@@ -9,6 +9,9 @@ var Twit = require('twit');
 var exec = require('child_process').exec;
 var fs = require('fs');
 
+// Request for downloading files
+var request = require('request');
+
 // // Pass object to twit package.
 const T = new Twit( {
 	consumer_key: process.env.CONSUMER_KEY,
@@ -17,7 +20,10 @@ const T = new Twit( {
 	access_token_secret: process.env.ACCESS_TOKEN_SECRET
 } );
 
-tweetIt();
+// Get the twitter user stream (for responses to bot).
+const stream = T.stream('user');
+
+// tweetIt();
 
 function tweetIt() {
 	const command = 'processing-java --sketch=`pwd`/assets --run';
@@ -33,7 +39,7 @@ function tweetIt() {
 
 		T.post('media/upload', { media_data: base64 }, uploaded);
 
-		function uploaded(err, data, response) {
+		function uploaded( err, data, response ) {
 
 			const id = data.media_id_string;
 			const tweet = {
@@ -43,7 +49,7 @@ function tweetIt() {
 			T.post('statuses/update', tweet, tweeted);
 		} // end uploaded
 
-		function tweeted(err, data, response) {
+		function tweeted( err, data, response ) {
 			if (err) {
 				console.log( "Something went wwrong!" );
 			} else {
@@ -51,4 +57,69 @@ function tweetIt() {
 			}
 		} // end tweeted
 	} // end processing()
+} // end tweetIt
+
+// Create an event when someone tweets bot.
+stream.on( 'tweet', convoTest );
+
+function convoTest( tweet ) {
+
+	// What's the deal with this tweet?
+	const reply_to = tweet.in_reply_to_screen_name;
+	const name = tweet.user.screen_name;
+	const txt = tweet.text;
+	const media = tweet.entities.media;
+	const id = tweet.id_str;
+
+	// console.log(reply_to);
+	// console.log(name);
+	// console.log(txt);
+	// console.log(id);
+
+	// You talking to me?
+	if ( reply_to === 'NiceNiceBot' ) {
+
+		// What happens if there's no <img>?
+		if ( media === undefined ) {
+			const reply = '@' + name + ' I need an image to do something!';
+			T.post( 'statuses/update', {
+				status: reply,
+				in_reply_to_status_id: id
+			}, tweeted );
+
+		// But if there is an image, get it.
+		} else if ( media.length > 0 ) {
+			const img = media[0].media_url;
+			downloadMedia( img, 'media' );
+		}
+	} // endif
+
+	// Snag the media.
+	function downloadMedia( url, filename ) {
+		console.log( 'I\'m going to try to download the file for you, okay?' );
+
+		console.log( 'Trying to download url: ' + url + ' to ' + filename );
+
+		// Make OAUTH request.
+		// request.head( url, downloaded );
+
+		function downloadComplete() {
+
+		} // downloadComplete
+	} // end downloadMedia
+
+	// TODO: What to do once the <img> is downloaded
+	// TODO: Put in processing folder
+	// TODO: Do something to it w/ Processing
+	// TODO: Reupload back to twitter
+	// TODO: Send back to user / post?
+
+} // end convoTest
+
+function tweeted( err, success ) {
+	if ( err !== undefined ) {
+		console.log( err ) ;
+	} else {
+		console.log( 'Tweeted: ' + success.text );
+	}
 }
